cf028fac9023b97e63d1b0295975d86a94f847de
Raihan567 <aburaihansarker567@gmail.com>
2022-03-14 16:15:06 +0600

    added a headline on inventory page

--- src/components/inventory/Inventory.js
+++ src/components/inventory/Inventory.js
@@ -4,6 +4,8 @@ const Inventory = () => {
   return (
     <div>
       <h4>This is Inventory pages and some code write hare for fixed a new bug</h4>
+      <h3>Hello world this is Raihan</h3>
+      
     </div>
   );
 };
